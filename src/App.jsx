43d3b8d0e858509535007b78126aa3b3acec426f
HPSdevs <hpsdevs@gmail.com>
2024-07-16 06:42:06 +0200

    240716 update

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,29 @@
 import { useState,useEffect} from "react";
  
 export default function App() {
-  const [buttondisabled, chgbuttonDisabled] = useState(false); // dont change config in running
-  const [champs,setChamps]   = useState ();            // OBJ le champs par lui même
-  const [nbflag,setNbflag]   = useState (0);              // nombre de drapeaux disponible
-  const [nbmine,setNbmine] = useState (0);              // nombre de mines à trouver
-  const [status,setStatus]   = useState (0);             // statuts du jeu  (0=arret/init,1=marche,2=loose,3=win)
-  const [niveau,setNiveau]   = useState (1);             // niveau du jeu  ( % de mines / surface)
-  const [taille,setTaille]   = useState (1);            // taille du champs de bataille
-  const [tool  ,setTool]     = useState (0);             // choix de l'outils donne  0 = pioche, 1=drapeau, 2=interrogation
-  const [temps ,setTemps ]   = useState (0);             // temps de jeu, à chaque tic incremente ce compteur
-  const [zoom  ,setZoom ]   = useState (1);              // niv de zoom du jeu
-  const [action,SetAction]   = useState ([]);            // tableau de actions à faire (search tout autour)
-  const etatjeu = [ "jeu en Arrêt"," jeu en Marche","vous avez Perdu","vous avez Gagné"];   // se basant sur status
-  const etattool= [ "Pioche","Drapeau","interrogation"]; // se basant sur tool
-  const search  = [ [0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]]; // sequence de search tout autour
-  const contenu = {vu: false, affiche:false,  bombe:false }   // chaque case du champs contient
+  const [buttondisabled, chgbuttonDisabled] = useState(false); 
+  const [champs,setChamps]   = useState ();                    
+  const [nbflag,setNbflag]   = useState (0);                   
+  const [nbmine,setNbmine]   = useState (0);                   
+  const [status,setStatus]   = useState (0);                   
+  const [niveau,setNiveau]   = useState (1);                   
+  const [taille,setTaille]   = useState (5);                   
+  const [tool  ,setTool]     = useState (0);                   
+  const [temps ,setTemps ]   = useState (0);                   
+  const [action,SetAction]   = useState ([]);                  
+  const etatjeu = [ "jeu en Arrêt"," jeu en Marche","vous avez Perdu","vous avez Gagné"];   
+  const etattool= [ "Pioche","Drapeau","interrogation"]; 
+  const autour  = [ [0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]]; 
+  const contenu = {vu: false, affiche:false,  bombe:false }   
 
-  // CHAMPS: contenu des différentes cases du champs de bataille :
-  //   x  ,  y ,  casedecouverte (0/1) , affichage( rien = 0 , chiffre = {1 à 8}, (F) drapeau, (?) interrogation, mine)
-  // le chiffre affiché donne le nombre de mines qui sont adjacentes à la case du chiffre.
-  // ACTION: contenu des actions
-  //  je clic sur  x ,  y  =>  verifier que cas  
-  //  x+1 , y-1 => verifier  // x+1 , y => verifier  //  x+1 , y+1 => verifier  //  x , y+1 => verifier   //  x-1 , y+1 => verifier
-  //  x-1 , y => verifier   //  x-1 , y-1 => verifier  // donc memoriser X,Y du clic si doigt pour rechercher case et tout autour
+
 
   useEffect(()=>{
-  const field = Array.from(Array(taille*5), () => new Array(taille*5).fill("no"));
+  const field = Array.from(Array(taille), () => new Array(taille).fill({sol:"herbe",terre:"",nb: 0}));
   setChamps(field);
   },[taille])
 
   // temps
-  setTimeout(() => { compter() }, 1000);
   function compter(){
     if (status===1){setTemps((temps) => ++temps )}
   }
@@ -60,43 +52,69 @@ export default function App() {
   function handleTool(x){
       setTool(x);
   }
-  // functions de base zoom (agrandissement visuel), niveau (% de nombre de mines), taille du champs (nombre de cases)
-  function handleZoom(x){
-      setZoom((zoom)=> ( (zoom===1 && x<0)? zoom : (zoom===5 && x>0) ? zoom : zoom+x))
-  }
   function handleNiveau(x){
       setNiveau((niveau)=> ( (niveau===1 && x<0)? niveau : (niveau===9 && x>0) ? niveau : niveau+x))
   }
   function handleTaille(x){
-      setTaille((taille)=> ( (taille===1 && x<0)? zoom : (taille===5 && x>0) ? taille : taille+x))
+      setTaille((taille)=> ( (taille===5 && x<1)? taille : (taille===20 && x>1) ? taille : taille*x))
   }
   /////////////////////////////////////////////
   // Le jeu
   function initialisation(){
-    const surface = (taille*5)**2;                                         // nombre de cases
-    const nbm = Math.floor(surface*niveau/10+Math.random()*5);              // % de mines par rapport aux cases
+    const surface = taille**2;                                              // nombre de cases
+    const nbm = Math.floor(surface*niveau/10+Math.random()*niveau);              // % de mines par rapport aux cases
     setNbflag(nbm);                                                         // nombre de drapeau disponible
     setNbmine(nbm);                                                         // nombre de mines
     MettreMines();                                                          // parsemer le champ de mines
   }
-
+ 
 
   // affectation aléatoire des mines
   function MettreMines(){
-    const field = Array.from(Array(taille*5), () => new Array(taille*5).fill("no"));
+    const field = Array.from(Array(taille), () => new Array(taille).fill({sol:"herbe",terre:" ",nb:0}));
     for (let i = 0; i < nbmine;i++) {
       let c,x,y = null;
       do { 
-          x= Number(Math.floor( Math.random()*taille*5));
-          y= Number(Math.floor( Math.random()*taille*5));
-          c= field[x][y];
-      } while (c!="no")  // ne pas mettre une mine là ou il y en a déjà une ! sinon BOOOM!!!!
-      field[x][y]="bb";
-    }
-    setChamps(field);
+          x= Number(Math.floor( Math.random()*taille));
+          y= Number(Math.floor( Math.random()*taille));
+          c= field[y][x].terre;
+        } while (c==="Mine")  // ne pas mettre une mine là ou il y en a déjà une ! sinon BOOOM!!!!
+        autour.forEach((pos)=>{ 
+          const xx = x+pos[0]<0 ? "OUT" : x+pos[0]>(taille-1) ? "OUT" : x+pos[0];
+          const yy = y+pos[1]<0 ? "OUT" : y+pos[1]>(taille-1) ? "OUT" : y+pos[1];
+          const zz = xx!="OUT" && yy!="OUT";
+            if (zz) { 
+              let old= field[yy][xx];
+              field[yy][xx] = {...old, nb: old.nb+1};
+            } 
+          }
+        )
+        field[y][x]={sol:"herbe",terre:"Mine", value: 0};
+      }
+      setChamps(field);
   }
+  function Look(x,y){
+    if (status===1){
+    console.log ("⚠️look",x,y)
+    const vue= champs[y][x];
+    console.log ("sol:",vue.sol,"terre:",vue.terre,"nb:", vue.nb)
+    if (vue.terre==="Mine" & tool===0){ setStatus(2)}
+    if (vue.sol==="herbe" & tool===1){ setsol(x,y,"flag")}
+    }
 
 
+    
+
+
+  }
+  
+  function setsol(x,y,objet){
+    console.log ("set",x,y, objet)
+    const field= champs;
+    field[y][x] = {sol:objet};
+    setChamps(field);
+    
+  }
   return (
     <>
       <h1>** Bienvenue sur ReactDmineur **</h1>
@@ -104,20 +122,29 @@ export default function App() {
       <h4>©2024 by HPSdevs - version 240715.01.00</h4>
       <p> Compteur  de jeu  : {temps}</p>
       <p><button onClick={()=>handleStart()}>START/RESET</button>&nbsp;status du jeu  : {etatjeu[status]} </p>
-      {/* <p><button onClick={()=>handleZoom(-1)}>-</button>&nbsp;Zoom du jeu: {zoom}&nbsp;<button onClick={()=>handleZoom(1)}>+</button></p> */}
-      <p><button disabled={buttondisabled}  onClick={()=>handleNiveau(-1)}>-</button>&nbsp;Niveau de jeu (%mines): {niveau}&nbsp;<button onClick={()=>handleNiveau(1)} disabled={buttondisabled} >+</button></p>
-      <p><button disabled={buttondisabled}  onClick={()=>handleTaille(-1)}>-</button>&nbsp;Taille champs (Nb cases): {taille}&nbsp;<button onClick={()=>handleTaille(1)} disabled={buttondisabled} >+</button></p>
-      <p> OUTILS:&nbsp; 
-      <button onClick={()=>handleTool(0)}>Pioche {tool===0 && "en cours"}</button>&nbsp;
-      <button onClick={()=>handleTool(1)}>Drapeau {tool===1 && "en cours"} ({nbflag} dispo)</button>&nbsp;
-      <button onClick={()=>handleTool(2)}>Interrogation {tool===2 && "en cours"}</button>
+      <p>
+        <button disabled={buttondisabled}  onClick={()=>handleNiveau(-1)}>-</button>
+          &nbsp;Niveau de jeu (%mines): {niveau}&nbsp;
+        <button onClick={()=>handleNiveau(1)} disabled={buttondisabled} >+</button>
+      </p>
+      <p>
+        <button disabled={buttondisabled}  onClick={()=>handleTaille(.5)}>-</button>
+          &nbsp;Taille champs (Nb cases): {taille}&nbsp;
+        <button onClick={()=>handleTaille(2)} disabled={buttondisabled} >+</button>
+      </p>
+      <p>OUTILS:&nbsp; 
+        <button onClick={()=>handleTool(0)}>Pioche {tool===0 && "en cours"}</button>&nbsp;
+        <button onClick={()=>handleTool(1)}>Drapeau {tool===1 && "en cours"} ({nbflag} dispo)</button>&nbsp;
+        <button onClick={()=>handleTool(2)}>Interrogation {tool===2 && "en cours"}</button>
       </p>
       <div className="champ">
         <>
+          {status===2 && <div className="info"><h3>PERDU</h3></div>}
           {champs && champs.map((row, j) =>   
-            <div key={j}>
-            {row.map((trou, i) => ( <div className={trou}>{i},{j}</div> ) )}    
-            </div>
+            <ul key={j}>
+            {row.map((carre, i) => ( 
+              <li key={j*100+i} className="herbe" onClick={()=>Look(i,j)}>{carre.nb>0 && carre.nb} {carre.terre}</li> ) )}    
+            </ul>
           )}
         </>
       </div>    
